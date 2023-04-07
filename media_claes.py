ae50d89cde3e2dd58f8f4d36848c4cfc00a5bef8
Daniel Valencia Gonzalez <106853804+DavsProg@users.noreply.github.com>
2023-04-06 21:24:36 -0500

    Create media_claes.py

---
+++ media_claes.py
@@ -0,0 +1,6 @@
+def better_than_average(class_points, your_points):
+    # Calcula el promedio sumando todos los puntos y dividiéndolos por el número de estudiantes, incluyéndote a ti.
+    average = sum(class_points + [your_points]) / len(class_points) + 1
+    
+    # Devuelve True si tu puntaje es mayor que el promedio, de lo contrario, devuelve False.
+    return your_points > average
