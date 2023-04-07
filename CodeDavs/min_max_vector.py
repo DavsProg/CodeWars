8c428521d797fcfb3d1d3b3ce540256543e39a42
DavsProg <you@example.com>
2023-04-07 14:57:35 -0500

    ejemplos basicos de vectores

---
+++ CodeDavs/min_max_vector.py
@@ -0,0 +1,7 @@
+def minimum(arr):
+    """Esta función recibe una lista de enteros como entrada y devuelve el número más pequeño de esa lista."""
+    return min(arr)
+
+def maximum(arr):
+    """Esta función recibe una lista de enteros como entrada y devuelve el número más grande de esa lista."""
+    return max(arr)
